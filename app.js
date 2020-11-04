bb36d47aa46ea7a5020407c3136ae3467e789386
cwc329 <iaiminwtg@gmail.com>
2020-11-05 00:37:47 +0800

    set middlewares

--- app.js
+++ app.js
@@ -1,20 +1,25 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const flash = require('connect-flash');
+const port = pross.env.PORT || 4001;
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
 
-var app = express();
+const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(flash());
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
+app.listen(port, () => {
+  console.log(`listening to port ${port} now...`)
+})
 
 module.exports = app;
